ed523dc727576f3c3d37643a6956ab007f7d6e82
drytuna <drytuna@gmail.com>
2014-07-15 08:12:45 -0700

    Circus.py Time:O(nlogn) Space:O(n)

--- Circus.py
+++ Circus.py
@@ -1,27 +1,32 @@
 def build_tower(people):
-    w_dict = sort_list(people, 0)   # sorted dict by weight
-    h_dict = sort_list(people, 1)   # sorted dict by height
-    r_dict = {}
+    r_dict = sort_list(people)
     result = []
-    for i in w_dict:
-        # building a dictionary with value as rank
-        r_dict[i] = w_dict[i] + h_dict[i] + min(w_dict[i], h_dict[i])
-    r_dict = sorted(r_dict, key = lambda x: -r_dict[x])
     x = y = float('inf')
     for (w, h) in r_dict:
         if w <= x and h <= y:
             result.append((w, h))
             x, y = w, h
-    print str(r_dict)
     return result
 
 
-def sort_list(people, index):
+def sort_list(people):
     x = {}
-    y = sorted(people, key = lambda x: x[index])
+    # sort by first index
+    y = sorted(people, key = lambda x: x[0])
     for i in range(len(people)):
-        x[y[i]] = i
-    return x
+        x[y[i]] = [i, 0]
+
+    # sort by second index
+    y = sorted(people, key = lambda x: x[1])
+    for i in range(len(people)):
+        x[y[i]][1] = i
+
+    # compute rank for every pair
+    for i in x:
+        x[i] = x[i][0] + x[i][1] + min(x[i][0], x[i][1])
+
+    # return sorted list in descending rank order
+    return sorted(x, key = lambda i: -x[i])
 
 
 if __name__ == "__main__":
@@ -35,4 +40,11 @@ if __name__ == "__main__":
               (8, 9),
               (1, 12),
               (12, 1)]
+    print str(build_tower(people))
+
+    z = 8
+    people = []
+    for i in range(z):
+        for j in range(z):
+            people.append((i, j))
     print str(build_tower(people))
