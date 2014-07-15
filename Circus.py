489a2433a63f3a15017770d6b9cac99f858f5cef
drytuna <drytuna@gmail.com>
2014-07-14 18:26:08 -0700

    added a blocker and resolved

--- Circus.py
+++ Circus.py
@@ -1,45 +1,19 @@
 def build_tower(people):
-    w_sort = sort_list(people, 0)
-    h_sort = sort_list(people, 1)
-    w_dict = {}
-    h_dict = {}
+    w_dict = sort_list(people, 0)   # sorted dict by weight
+    h_dict = sort_list(people, 1)   # sorted dict by height
+    r_dict = {}
     result = []
-    for i in range(len(people)):
-        w_dict[w_sort[i]] = i
-    for i in range(len(people)):
-        h_dict[h_sort[i]] = i
-    w_index = h_index = len(people)-1
-    while w_index > -1 and h_index > -1:
-        min_w_i = min_index(w_sort[w_index], w_dict, h_dict)
-        min_h_i = min_index(h_sort[h_index], w_dict, h_dict)
-        if min_w_i > min_h_i:
-            if min_h_i > -1:
-                result.append(w_sort[w_index])
-                if min_w_i == 0:
-                    break
-                w_dict[w_sort[w_index]] = -1
-                h_dict[w_sort[w_index]] = -1
-                w_dict[h_sort[h_index]] = -1
-                h_dict[h_sort[h_index]] = -1
-                w_index -= 1
-                h_index -= 1
-            else:
-                h_index -= 1
-        else:
-            if min_w_i > -1:
-                result.append(h_sort[h_index])
-                if min_h_i == 0:
-                    break
-                w_dict[h_sort[h_index]] = -1
-                h_dict[h_sort[h_index]] = -1
-                w_dict[w_sort[w_index]] = -1
-                h_dict[w_sort[w_index]] = -1
-                w_index -= 1
-                h_index -= 1
-            else:
-                w_index -= 1
+    for i in w_dict:
+        # building a dictionary with value as rank
+        r_dict[i] = w_dict[i] + h_dict[i] + min(w_dict[i], h_dict[i])
+    r_dict = sorted(r_dict, key = lambda x: -r_dict[x])
+    x = y = float('inf')
+    for (w, h) in r_dict:
+        if w <= x and h <= y:
+            result.append((w, h))
+            x, y = w, h
+    print str(r_dict)
     return result
-
 
 def min_index(item, dict1, dict2):
     if dict1[item] < dict2[item]:
@@ -48,7 +22,11 @@ def min_index(item, dict1, dict2):
         return dict2[item]
 
 def sort_list(people, index):
-    return sorted(people, key = lambda x: x[index])
+    x = {}
+    y = sorted(people, key = lambda x: x[index])
+    for i in range(len(people)):
+        x[y[i]] = i
+    return x
 
 
 if __name__ == "__main__":
@@ -59,5 +37,7 @@ if __name__ == "__main__":
               (5, 8),
               (6, 7),
               (7, 7.5),
-              (8, 9)]
+              (8, 9),
+              (1, 12),
+              (12, 1)]
     print str(build_tower(people))
